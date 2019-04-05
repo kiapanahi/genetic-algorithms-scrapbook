b711ef76bebf0a9f173d30a19e0c9b6d065563c9
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-04-05 06:29:20 +0430

    Move Individual to its own file

--- src/shakespeare-and-monkey/Program.cs
+++ src/shakespeare-and-monkey/Program.cs
@@ -17,34 +17,4 @@ namespace ShakespeareAndMonkey
         * randomize them
         ...
      */
-
-    /*
-    individual: the entity that
-        * can calculate it's fitness
-        * knows about it's gene[s]
-        * can crossover with another individual
-        * can apply mutation to it's gene[s]
-     */
-    public class Individual
-    {
-        public char[] GenomePool { get; private set; }
-        public double Fitness { get; private set; }
-
-        public Individual(int characterCount)
-        {
-            GenomePool = new char[characterCount];
-        }
-
-        public void CalculateFitness()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Individual CrossOver(Individual other)
-        {
-            throw new NotImplementedException();
-        }
-
-    }
-
 }
