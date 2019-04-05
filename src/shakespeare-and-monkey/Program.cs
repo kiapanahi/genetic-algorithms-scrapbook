8b531178909897be14f953e02c90ef07d2d03dd4
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-04-05 06:25:41 +0430

    Add basic implementation of Individual entity

--- src/shakespeare-and-monkey/Program.cs
+++ src/shakespeare-and-monkey/Program.cs
@@ -25,5 +25,26 @@ namespace ShakespeareAndMonkey
         * can crossover with another individual
         * can apply mutation to it's gene[s]
      */
+    public class Individual
+    {
+        public char[] GenomePool { get; private set; }
+        public double Fitness { get; private set; }
+
+        public Individual(int characterCount)
+        {
+            GenomePool = new char[characterCount];
+        }
+
+        public void CalculateFitness()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Individual CrossOver(Individual other)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 
 }
