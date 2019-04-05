984f05b54d2a82bf47c65c788a70eb07d257d87a
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-04-05 05:46:41 +0430

    Add comments about what entities should be created

--- src/shakespeare-and-monkey/Program.cs
+++ src/shakespeare-and-monkey/Program.cs
@@ -10,4 +10,20 @@ namespace ShakespeareAndMonkey
 
         }
     }
+
+    /*
+    population: a collection of individuals
+        * initialize them
+        * randomize them
+        ...
+     */
+
+    /*
+    individual: the entity that
+        * can calculate it's fitness
+        * knows about it's gene[s]
+        * can crossover with another individual
+        * can apply mutation to it's gene[s]
+     */
+
 }
